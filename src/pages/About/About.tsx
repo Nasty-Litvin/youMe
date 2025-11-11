bf60e95d22e4692121bac4d8cb7b3353d921cbfa
nasty <nastylitvin1991liverpool@gmail.com>
2025-11-11 23:52:26 +0300

    Added Title in the About

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,11 +1,21 @@
+import Title from "../../components/Title/Title";
+
 import "./style.scss";
 
+const titleText = "В YouMi вы не одни Вместе мы справимся";
+
 const About = () => {
-    return ( 
-        <div className="about">
-            About
-        </div>
-     );
-}
- 
+  return (
+    <div className="about">
+      <div className="about__title">
+        <Title
+          text={titleText}
+          style={{ fontSize: "44px", lineHeight: "130%", maxWidth: "640px" }}
+        />
+        <img src="/about/aboutMain.png" alt="#" />
+      </div>
+    </div>
+  );
+};
+
 export default About;
